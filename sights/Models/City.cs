4a55c5d3654d20fc960ff6746628942d498ad698
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2023-01-17 16:22:32 +0100

    Database column names update

--- sights/Models/City.cs
+++ sights/Models/City.cs
@@ -5,11 +5,17 @@ namespace sqlite.Models;
 
 public partial class City
 {
-    public byte[] Id { get; set; } = null!;
+    public long Id { get; set; }
 
-    public long? UserId { get; set; }
+    public string Name { get; set; } = null!;
 
-    public string? Name { get; set; }
+    public long UserId { get; set; }
 
-    public long? CountryId { get; set; }
+    public long CountryId { get; set; }
+
+    public virtual ICollection<Attraction> Attractions { get; } = new List<Attraction>();
+
+    public virtual Country Country { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
 }
--- sights/Models/Like.cs
+++ sights/Models/Like.cs
@@ -5,11 +5,15 @@ namespace sqlite.Models;
 
 public partial class Like
 {
-    public byte[] Id { get; set; } = null!;
+    public long Id { get; set; }
 
-    public long? UserId { get; set; }
+    public long UserId { get; set; }
 
-    public long? AttractionId { get; set; }
+    public long AttractionId { get; set; }
 
-    public byte[]? Like1 { get; set; }
+    public long? Like1 { get; set; }
+
+    public virtual Attraction Attraction { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
 }
