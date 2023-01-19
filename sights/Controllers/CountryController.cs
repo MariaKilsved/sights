9517a9d26d7e44377f72af33378894174397928e
Littecke <89915867+Littecke@users.noreply.github.com>
2023-01-19 13:20:10 +0100

    Added controllers for all models

---
+++ sights/Controllers/CountryController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sqlite.Data;
+using sqlite.Models;
+
+namespace sights.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly SqliteContext _context;
+
+        public CountryController(SqliteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Country
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound();
+          }
+            return await _context.Countries.ToListAsync();
+        }
+
+        // GET: api/Country/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(long id)
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound();
+          }
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        // PUT: api/Country/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(long id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Country
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+          if (_context.Countries == null)
+          {
+              return Problem("Entity set 'SqliteContext.Countries'  is null.");
+          }
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Country/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(long id)
+        {
+            if (_context.Countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(long id)
+        {
+            return (_context.Countries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
