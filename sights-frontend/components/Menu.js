56fdaa96ba15a461727499c07e208fc2a4759ffd
fransnl <nl.frans@gmail.com>
2023-01-19 10:47:44 +0100

    added colors, buttons

--- sights-frontend/components/Menu.js
+++ sights-frontend/components/Menu.js
@@ -1,5 +1,25 @@
+import primaryButton from "./primaryButton.js";
+import secondaryButton from "./secondaryButton.js";
+
 export default function Menu(){
     const menu = document.createElement('div');
     menu.id = 'menu';
+
+    const logIn = primaryButton('a');
+    logIn.innerHTML = 'login';
+    logIn.href = '/login'
+    
+    const signup = secondaryButton('a');
+    signup.innerHTML = 'signup';
+    signup.href = '/register'
+
+    const logo = document.createElement('img');
+    logo.id = 'logo';
+    logo.src = '/icons/Logo.svg'
+
+    menu.append(logo);
+    menu.append(logIn);
+    menu.append(signup);
+
     return menu;
 }
---
+++ sights-frontend/components/primaryButton.js
@@ -0,0 +1,5 @@
+export default function primaryButton(tag){
+    const btn = document.createElement(tag);
+    btn.id = 'primary-btn';
+    return btn
+}
