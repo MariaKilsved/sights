c5714f59c840e6a751a740a0eb28ad77569123e5
Lunkan92 <89257437+Lunkan92@users.noreply.github.com>
2023-01-19 09:33:28 +0100

    Updated login and accountpage

---
+++ sights-frontend/add-sights/addsights.js
@@ -0,0 +1,42 @@
+'use strict';
+import Menu from '../components/Menu.js'
+import Card from "../components/Card.js";
+window.addEventListener("DOMContentLoaded", async () => {
+    addSight();
+});
+
+function addSight(){
+    const page = document.getElementById('page');
+    const card = Card();
+    const menu = Menu();  
+    const addSight = document.createElement('p2');
+    addSight.innerHTML = 'Add Sight';
+    card.append(addSight)
+
+    const inputHeadline= document.createElement("input");
+    inputHeadline.setAttribute("type", "text");
+    inputHeadline.placeholder ="Headline"
+    card.append(inputHeadline);
+
+    const inputCountry = document.createElement("input");
+    inputCountry.setAttribute("type", "text");
+    inputCountry.placeholder ="Country"
+    card.append(inputCountry);
+
+    const inputCity = document.createElement("input");
+    inputCity.setAttribute("type", "text");
+    inputCity.placeholder ="City"
+    card.append(inputCity);
+
+    const inputDescription = document.createElement("input");
+    inputDescription.setAttribute("type", "text");
+    inputDescription.placeholder ="Description"
+    card.append(inputDescription);
+ 
+
+
+    page.append(card);
+    page.append(menu);
+    
+
+}
