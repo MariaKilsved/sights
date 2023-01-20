01e34d6ec1c809e450020851ddb48a56569aed6a
fransnl <nl.frans@gmail.com>
2023-01-20 11:29:39 +0100

    fixed CORS

--- sights/Program.cs
+++ sights/Program.cs
@@ -4,6 +4,7 @@ using sqlite.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -27,6 +28,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+// global cors policy - the call to UseCors() must be done here
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.UseAuthorization();
 
