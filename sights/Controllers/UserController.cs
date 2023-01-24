ea486208a4748cdb9e5ca6bc94aae89ba349ef75
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2023-01-24 09:46:28 +0100

    Changed login to return an IActionResult instead of bool.

--- sights/Controllers/UserController.cs
+++ sights/Controllers/UserController.cs
@@ -122,10 +122,34 @@ namespace sights.Controllers
         }
 
         [HttpGet("Login")]
-        public async Task<bool> Login(string username, string password)
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Login(string username, string password)
         {
-            IEnumerable<User> Users = await _context.Users.Where(u => u.Username == username && u.Password == password).ToListAsync();
-            return Users.Any();
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+
+            //Testing for username only first
+            IEnumerable<User> Users1 = await _context.Users.Where(u => u.Username == username).ToListAsync();
+
+            if (!Users1.Any())
+            {
+                return NotFound("Username is incorrect or user does not exist.");
+            }
+
+            //Testing for both username and password
+            IEnumerable<User> Users2 = await _context.Users.Where(u => u.Username == username && u.Password == password).ToListAsync();
+
+            if (!Users2.Any())
+            {
+                return BadRequest("Password is incorrect.");
+            }
+
+            return NoContent();
+
         }
     }
 }
