72015eba63ba0d9ab45ff3b2d8eab65f8f454ab4
Lunkan92 <89257437+Lunkan92@users.noreply.github.com>
2023-01-26 13:49:20 +0100

    Changed link on pages

--- sights-frontend/sights/sights.js
+++ sights-frontend/sights/sights.js
@@ -2,25 +2,49 @@
 import Menu from '../components/Menu.js';
 
 window.addEventListener("DOMContentLoaded", async () => {
-    render();
+    const queryString = window.location.search;
+    const location = new URLSearchParams(queryString).get('location')
+
+    const sights = await fetch('https://localhost:7260/api/Attraction').then(res => res.json());
+    const countries = await fetch('https://localhost:7260/api/Country').then(res => res.json());
+    const cities = await fetch('https://localhost:7260/api/City').then(res => res.json());
+    
+    console.log(location);
+    render(location,sights,countries,cities);
+
 
     
 });
 
 
-function render(){
-    
+function render(location,sights){
+
+  
     const page = document.getElementById('page');
     const menu = Menu();
-   
+    const name = document.createElement('p');
+    name.innerHTML=location;
 
-    const name = document.getElementById('p');
+    
+    sights.forEach((attraction) => {
+        const row = document.createElement('tr')
+        
+        const title = document.createElement('td');
+        const link = document.createElement('a');
+        link.className = 'link';
+        link.innerHTML = attraction.title;
+        link.href = `/comment/?id=${attraction.id}`;
+        
+        title.append(link);
+        row.append(title);
+        sights.append(row);
+    });
+
   
 
-
-
     page.append(name);
-    page.append(card);
+    page.append(menu);
+    
 
  
     
