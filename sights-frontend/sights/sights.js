270b0203651d8465ce516baa767802e2c06675c3
Lunkan92 <89257437+Lunkan92@users.noreply.github.com>
2023-01-26 12:54:34 +0100

    Changed name on every page

--- sights-frontend/sights/sights.js
+++ sights-frontend/sights/sights.js
@@ -1,14 +1,28 @@
 'use strict';
+import Menu from '../components/Menu.js';
 
 window.addEventListener("DOMContentLoaded", async () => {
     render();
+
+    
 });
+
 
 function render(){
     
     const page = document.getElementById('page');
-    const hello = document.createElement('p');
-    hello.innerHTML = 'hello';
-    page.append(hello);
+    const menu = Menu();
+   
+
+    const name = document.getElementById('p');
+  
+
+
+
+    page.append(name);
+    page.append(card);
+
+ 
+    
 
 }
