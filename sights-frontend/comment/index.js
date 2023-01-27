0887338dc20726661c9ae72f84b5960644b051ac
Lunkan92 <89257437+Lunkan92@users.noreply.github.com>
2023-01-27 15:52:54 +0100

    update

--- sights-frontend/comment/index.js
+++ sights-frontend/comment/index.js
@@ -4,100 +4,62 @@ import Menu from "../components/Menu.js";
 import Sight from "../components/Sight.js";
 import CommentBox from '../components/CommentBox.js'
 import AddCommentBox from "../components/AddCommentBox.js";
-import { post, get } from '../lib/request.js';
+import { post, get, deleteRequest, putRequest } from '../lib/request.js';
 import commentSamplePostData from './mock-data/MockData.js'
 
-// let downVoteNr = 0;
-
-let canDownVote = true;
-let canLike;
-
 window.addEventListener("DOMContentLoaded", async () => {
-   await render();
+    
+    let likes = await get(`https://localhost:7260/api/Like`);
+    const queryString = window.location.search;
+    const attractionId = new URLSearchParams(queryString).get('id');
+    const attraction = await get(`https://localhost:7260/api/Attraction/${attractionId}`)
+    const user = JSON.parse(localStorage.getItem('userinfo'));
+    let isLiked;
+    if(user){
+        isLiked = await hasLiked(user, attractionId, likes);
+    }
+    console.log(isLiked)
    
+    await render(user, isLiked, attraction);
 
-   //UPVOTE
-
-    const upVote = document.getElementById('likesUpIMG');
-    upVote.addEventListener('click', async () => {
-        let upVote = document.getElementById('likesUp');
-        const likes = await get(`https://localhost:7260/api/Like`);
-
-         const userInfo = JSON.parse(localStorage.getItem('userinfo'));
-         const queryString = window.location.search;
-         const attractionId = new URLSearchParams(queryString).get('id');
-
-         console.log(likes);
-
-        
-        likeLoop:
-        for (let i = 0; i < likes.length; i++){
-            if (likes[i].userId === userInfo.userId && attractionId == likes[i].attractionId){
-                if (likes[i].like1 == 0){
-                 await post(`https://localhost:7260/api/Like/${likes[i].id}`, userLike);
-                 break likeLoop;
-                }
-            }
-
-
-            if (i == likes.length -1){
+    if(user){
+        const upVote = document.getElementById('likesUpIMG');
+        upVote.addEventListener('click', async () => {
+            let upVote = document.getElementById('likesUp');
+    
+            if (!isLiked) {
                 const userLike = {
-                    userId: userInfo.userId,
-                    attractionId,
-                    like1: 1
+                    userId: user.userId,
+                    AttractionId: attractionId,
+                    like1: 1,
                 }
-              await post(`https://localhost:7260/api/Like`, userLike);
-            } 
-        }
-
-        if (canUpVote){
-        const userLike = {
-            userId: userInfo.userId,
-            AttractionId: attractionId,
-            like1: 1,
-        }
-            let upVoteNr = upVote.innerHTML;
-            upVoteNr++;
-            upVote.innerHTML = upVoteNr;
-            canUpVote = false;
-
-           return await post(`https://localhost:7260/api/Like`, userLike);
-        }
-        // else {
-        //     const userLike = {
-        //     userId: userInfo.userId,
-        //     AttractionId: attractionId,
-        //     like1: 0,
-        // }
-        //     let upVoteNr = upVote.innerHTML;
-        //     upVoteNr--;
-        //     upVote.innerHTML = upVoteNr;
-        //     canUpVote = true;
-
-        //     await post(`https://localhost:7260/api/Like`, userLike);
-        //     return;
-        // }
-    })
-
-
-
-    //DOWNVOTE
-
-    // const downVote = document.getElementById('likesDownIMG');
-    // downVote.addEventListener('click', async () => {
-    //     let downVote = document.getElementById('likesDown');
-
-    //     if (canDownVote){
-    //         downVoteNr++;
-    //         downVote.textContent = downVoteNr;
-    //         canDownVote = false;
-    //     }
-    //     else {
-    //         downVoteNr--;
-    //         downVote.textContent = downVoteNr;
-    //         canDownVote = true;
-    //     }
-    // })
+    
+                let upVoteNr = upVote.innerHTML;
+                upVoteNr++;
+                upVote.innerHTML = upVoteNr;
+    
+                await post(`https://localhost:7260/api/Like`, userLike);
+            }
+            else {
+                let upVoteNr = upVote.innerHTML;
+                upVoteNr--;
+                upVote.innerHTML = upVoteNr;
+                
+                let likeId;
+                likes.forEach(like =>{
+                    if(like.userId == user.userId && attractionId == like.attractionId){
+                        likeId = like.id;
+                    }
+                });
+    
+                await deleteRequest(`https://localhost:7260/api/Like/${likeId}?attractionId=${attractionId}&userId=${user.userId}`);
+            }
+    
+            likes = await get(`https://localhost:7260/api/Like`);
+            isLiked = hasLiked(user, attractionId, likes)
+            console.log(isLiked)
+        })
+    }
 
     const sendMessageBtn = document.getElementById('sendMessageBtn');
     sendMessageBtn.addEventListener('click', async () => {
@@ -123,7 +85,18 @@ window.addEventListener("DOMContentLoaded", async () => {
     })
 });
 
-async function render(){
+function hasLiked(user, attractionId, likes){
+    let hasLiked = false;
+    likes.forEach(like => {
+        if(like.userId == user.userId && attractionId == like.attractionId){
+            if(like.like1 === 1) hasLiked = true;
+        }
+    });
+
+    return hasLiked;
+}
+
+async function render(user, isLiked, attraction){
     const comments = await get(`https://localhost:7260/api/Comment`);
     const likes = await get(`https://localhost:7260/api/Like`);
     const queryString = window.location.search;
