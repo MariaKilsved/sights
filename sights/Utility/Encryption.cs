0f908ea1c3714b709f02f32530c51c0f958311b7
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2023-02-14 12:42:30 +0100

    Added encryption to API

---
+++ sights/Utility/Encryption.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace sights.Utility
+{
+    public static class Encryption
+    {
+        public static string HashPbkdf2(string password)
+        {
+            byte[] encrypted = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.UTF8.GetBytes("j78Y#p)/saREN!y3@"),
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 100,
+                numBytesRequested: 64);
+
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                builder.Append(encrypted[i].ToString("x2"));
+            }
+
+            string encryptedString = builder.ToString();
+
+            return encryptedString;
+        }
+    }
+}
