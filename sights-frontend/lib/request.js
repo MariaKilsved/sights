b4aedb69a5c65ad7e483a6b1bfe3ce93611f374f
Teologina <90504386+Teologina@users.noreply.github.com>
2023-02-14 10:45:08 +0100

    update fetch

--- sights-frontend/lib/request.js
+++ sights-frontend/lib/request.js
@@ -1,47 +1,73 @@
-export async function get(url){
+export async function get(url, jwtEncryptedToken = null){
+    let _headers = { 'Accept': 'application/json' };
+
+    if (jwtEncryptedToken != null){
+        _headers['Authorization'] = `Bearer ${jwtEncryptedToken}`;
+    }
+
     const response = await fetch( url , {
         method: 'GET',
         mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-          },
+        headers: _headers,
     }).then(res => res.json());
+
     return response;
 }
 
-export async function post(url, obj){
+export async function post(url, obj, jwtEncryptedToken = null){
+
+    let _headers =  {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          };
+
+    if (jwtEncryptedToken != null){
+        _headers['Authorization'] = `Bearer ${jwtEncryptedToken}`;
+    }
+
     const response = await fetch( url , {
         method: 'POST',
         mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
+        headers: _headers,
           body: JSON.stringify(obj)
     }).then(res => res.json())
     return response;
 }
 
-export async function deleteRequest(url){
+export async function deleteRequest(url, jwtEncryptedToken = null){
+
+        let _headers =  {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          };
+
+    if (jwtEncryptedToken != null){
+        _headers['Authorization'] = `Bearer ${jwtEncryptedToken}`;
+    }
+
     const response = await fetch( url , {
         method: 'DELETE',
         mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
+        headers: _headers,
     })
     return response;
 }
 
-export async function putRequest(url, obj){
+export async function putRequest(url, obj, jwtEncryptedToken = null){
+
+      let _headers =  {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+
+    if (jwtEncryptedToken != null){
+        _headers['Authorization'] = `Bearer ${jwtEncryptedToken}`;
+    }
+
     const response = await fetch( url , {
         method: 'PUT',
         mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
+        headers: _headers,
           body: JSON.stringify(obj)
     }).then(res => res.json())
     return response;
